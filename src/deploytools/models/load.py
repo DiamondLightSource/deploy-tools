8cc2c8a1ac1cd6ef5f91b58644db94bf8bc1f313
Martin Gaughran <martin.gaughran@diamond.ac.uk>
2024-07-16 19:50:47 +0000

    Require use of module name for configuration file paths

--- src/deploytools/models/load.py
+++ src/deploytools/models/load.py
@@ -14,6 +14,10 @@ T = TypeVar(
 CONFIG_FILENAME = "config.yaml"
 
 
+class LoadError(Exception):
+    pass
+
+
 def load_from_yaml(model: type[T], file_path: Path) -> T:
     with open(file_path) as f:
         return model(**yaml.safe_load(f))
@@ -36,16 +40,29 @@ def load_module_folder(folder: Path) -> ModuleConfig:
     return module
 
 
-def load_module_file(file: Path) -> ModuleConfig:
-    return load_from_yaml(ModuleConfig, file)
+def load_module(path: Path) -> ModuleConfig:
+    if path.is_dir():
+        return load_module_folder(path)
+    elif path.suffix == ".yaml":
+        return load_from_yaml(ModuleConfig, path)
+
+    raise LoadError(f"Unexpected file in configuration directory:\n{path}")
 
 
 def load_deployment(config_folder: Path) -> DeploymentConfig:
     modules: list[ModuleConfig] = []
-    for file in config_folder.glob("*"):
-        if file.is_dir():
-            modules.append(load_module_folder(file))
-        elif file.suffix == ".yaml":
-            modules.append(load_from_yaml(ModuleConfig, file))
+    for module_folder in config_folder.glob("*"):
+        if not module_folder.is_dir():
+            raise LoadError(f"Module path is not directory: {module_folder}")
+
+        for version_path in module_folder.glob("*"):
+            module = load_module(version_path)
+            if not module.metadata.name == module_folder.name:
+                raise LoadError(
+                    f"Module name {module.metadata.name} does not match path in "
+                    f"configuration directory:\n{module_folder}"
+                )
+
+            modules.append(module)
 
     return DeploymentConfig(modules=modules)
