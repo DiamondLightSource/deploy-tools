cbe6ca79ece26a7c1e8aa87f1ccc77966002e146
Martin Gaughran <martin.gaughran@diamond.ac.uk>
2025-02-03 13:37:17 +0000

    Move code for printing updates to separate module

--- src/deploy_tools/validate.py
+++ src/deploy_tools/validate.py
@@ -5,6 +5,7 @@ from tempfile import TemporaryDirectory
 
 from .build import build
 from .check_deploy import check_deploy_actions
+from .display import print_updates
 from .layout import Layout
 from .models.changes import DeploymentChanges, ReleaseChanges
 from .models.deployment import (
@@ -47,46 +48,6 @@ def validate_configuration(
 
         snapshot_default_versions = validate_default_versions(snapshot)
         print_updates(snapshot_default_versions, deployment_changes)
-
-
-def print_updates(
-    old_default_versions: DefaultVersionsByName, deployment_changes: DeploymentChanges
-) -> None:
-    print_module_updates(deployment_changes.release_changes)
-    print_version_updates(old_default_versions, deployment_changes.default_versions)
-
-
-def print_module_updates(release_changes: ReleaseChanges) -> None:
-    display_config = {
-        "deployed": release_changes.to_add,
-        "updated": release_changes.to_update,
-        "deprecated": release_changes.to_deprecate,
-        "restored": release_changes.to_restore,
-        "removed": release_changes.to_remove,
-    }
-
-    for action, releases in display_config.items():
-        print(f"Modules to be {action}:")
-
-        for release in releases:
-            print(f"{release.module.name}/{release.module.version}")
-
-        print()
-
-
-def print_version_updates(
-    old_defaults: DefaultVersionsByName, new_defaults: DefaultVersionsByName
-) -> None:
-    print("Updated module defaults:")
-    module_names = old_defaults.keys() | new_defaults.keys()
-
-    for name in module_names:
-        old = old_defaults.get(name, "None")
-        new = new_defaults.get(name, "None")
-        if not old == new:
-            print(f"{name} {old} -> {new}")
-
-    print()
 
 
 def validate_deployment_changes(
