9db7076585a1b0a882fbd9321a5e9b05c7b39c77
Giles Knap <giles.knap@diamond.ac.uk>
2025-04-24 05:34:13 +0000

    add BinaryApp app type

--- src/deploy_tools/models/parent.py
+++ src/deploy_tools/models/parent.py
@@ -4,4 +4,4 @@ from pydantic import BaseModel, ConfigDict
 class ParentModel(BaseModel):
     """Will forbid any extra parameters being provided in any subclass."""
 
-    model_config = ConfigDict(extra="forbid")
+    model_config = ConfigDict(extra="forbid", use_enum_values=True)
