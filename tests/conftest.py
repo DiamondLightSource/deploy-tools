2c5876f3f0ca9090f5880b6fbd9ba37813af7350
Martin Gaughran <martin.gaughran@diamond.ac.uk>
2025-06-30 15:04:25 +0000

    Update to latest Copier template

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,4 @@
-import os
 from pathlib import Path
-from typing import Any
 
 import pytest
 from typer.testing import CliRunner
@@ -15,24 +13,6 @@ def run_cli(*args):
     if result.exception:
         raise result.exception
     assert result.exit_code == 0, result
-
-
-# Prevent pytest from catching exceptions when debugging in vscode so that break on
-# exception works correctly (see: https://github.com/pytest-dev/pytest/issues/7409)
-if os.getenv("PYTEST_RAISE", "0") == "1":
-
-    @pytest.hookimpl(tryfirst=True)
-    def pytest_exception_interact(call: pytest.CallInfo[Any]):
-        if call.excinfo is not None:
-            raise call.excinfo.value
-        else:
-            raise RuntimeError(
-                f"{call} has no exception data, an unknown error has occurred"
-            )
-
-    @pytest.hookimpl(tryfirst=True)
-    def pytest_internalerror(excinfo: pytest.ExceptionInfo[Any]):
-        raise excinfo.value
 
 
 @pytest.fixture
