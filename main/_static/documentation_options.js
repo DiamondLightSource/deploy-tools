a0eb8c581d4b545a867e9fcaa3cde73c27be22be
MJGaughran <MJGaughran@users.noreply.github.com>
2026-07-15 10:32:00 +0000

    deploy: b5e0754b43649ef40e3997878e8b0b9ffb2709e4

--- main/_static/documentation_options.js
+++ main/_static/documentation_options.js
@@ -1,5 +1,5 @@
 const DOCUMENTATION_OPTIONS = {
-    VERSION: '0.7.2.dev75+g26c026f95',
+    VERSION: '0.7.2.dev92+gb5e0754b4',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
